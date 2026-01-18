b6ca66fe7f636cfd1ba59283661e9a390c584b42
renejsequeira <renej.sequeira@gmail.com>
2026-01-17 21:19:23 -0500

    Update Restaurant.java

--- src/firstAssignment/Restaurant.java
+++ src/firstAssignment/Restaurant.java
@@ -4,7 +4,7 @@ public class Restaurant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Rene");
+		System.out.print("Rene Sequueira");
 		
 	}
 
