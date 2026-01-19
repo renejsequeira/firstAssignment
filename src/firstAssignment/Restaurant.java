091c7f22feec61ce0c423147176a1ae966b344ff
renej <renej@Javys>
2026-01-18 19:57:15 -0500

    updated code

--- src/firstAssignment/Restaurant.java
+++ src/firstAssignment/Restaurant.java
@@ -1,10 +1,36 @@
+//Rene Sequeira
+//Pseudocode critical thinking assignment 1
+/*
+ * Start
+ * set restaurantName variable to name of restaurant
+ * set restaurantAddress variable to address of restaurant
+ * set city variable to city of restaurant location
+ * set state variable to state of restaurant location
+ * 
+ * display restaurant name, restaurant address, city, state, and zip-code
+ * end
+ */
+
+
+
+
+
 package firstAssignment;
 
 public class Restaurant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("R");
+		String restaurantName = "EL Toro Loco Steakhouse";
+		String restaurantAddress = "1970 Sw 8th St";
+		String city = "Miami";
+		String state = "Fl";
+		int zipCode = 33135;
+		
+		
+		System.out.print("\n\n"  + "\t" + restaurantName + "\n" +
+						 "\t" + restaurantAddress + "\n" +
+						 "\t" + city + "," + state + "," + zipCode);
 		
 	}
 
